5aba9665a4e839326566980ecab69fc9966f2cbb
Emilio Botier <70599464+EmilioBoti@users.noreply.github.com>
2025-03-08 18:15:37 +0100

    feat: added base-view

---
+++ ebotlib/src/main/java/com/ebot/ebotlib/component/BaseView.kt
@@ -0,0 +1,37 @@
+package com.ebot.ebotlib.component
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.HapticFeedbackConstants
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlin.math.sqrt
+
+
+open class BaseView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): View(context, attrs, defStyleAttr) {
+
+
+    @RequiresApi(Build.VERSION_CODES.O_MR1)
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let { touch ->
+            parent?.requestDisallowInterceptTouchEvent(true)
+            when(touch.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    this.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+                }
+                MotionEvent.ACTION_UP -> {
+                    this.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY_RELEASE)
+                }
+            }
+            return true
+        }
+        return false
+    }
+
+}
